76aa009fc2e37521a5ec68288f60c17c0dabc896
Alberto Soutullo Rendo <alberto.soutullo639@gmail.com>
2022-11-18 12:48:09 +0100

    Modify main and runner to work with the previous modification

--- src/main.py
+++ src/main.py
@@ -1,14 +1,27 @@
 # Python Imports
 import typer
 
+# Project Imports
+from src.simulation_runner.runner import run_simulation
+from src.utilities.files.json.simulation_config_parser import SimulationConfigParser
+from src.utilities.env_variables import input_settings
+
 
 def main(
         run_type: str = typer.Option(..., "--run", "-r"),
 ):
+    parser = SimulationConfigParser(input_settings)
+    arguments_config, simulation_config, plotter_config = parser.read_content()
     # Calls depending on json config
-    pass
+    if run_type == "simulation":
+        run_simulation(arguments_config, simulation_config)
+    elif run_type == "plotter":
+        # run_plotter()
+        pass
+    else:
+        run_simulation()
+        # run_plotter()
 
 
 if __name__ == '__main__':
-    print("test")
-    # typer.run(main)
+    typer.run(main)
--- src/simulation_runner/runner.py
+++ src/simulation_runner/runner.py
@@ -2,8 +2,15 @@
 import os
 
 # Project Imports
-from src.utilities.env_variables import binary_name
+from src.utilities.env_variables import binary_path, configuration_settings
+from src.utilities.files.json.json_utils import write_json
 
 
-def run_simulation(output_format, input_settings, output_file):
-    os.system(binary_name + " -f " + output_format + " -i " + input_settings + " -o " + output_file)
+def run_simulation(arguments_config: dict, simulation_config: dict):
+
+    write_json(simulation_config, configuration_settings)
+
+    os.system(binary_path
+              + " -f " + arguments_config["output-format"]
+              + " -i " + configuration_settings + " -o "
+              + arguments_config["output-file"])
