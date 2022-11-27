3d7f0ca783893f94f15ee85cbd57c0b41e3e1f75
Alberto Soutullo Rendo <alberto.soutullo639@gmail.com>
2022-11-27 18:57:31 +0100

    Added methods to read data from polars.

--- src/utilities/files/simulation_data_types/base_data_simulation_handler.py
+++ src/utilities/files/simulation_data_types/base_data_simulation_handler.py
@@ -1,16 +1,11 @@
+# Python Imports
 import abc
 from abc import ABC
+import polars as pl
 
 
 class BaseDataSimulationHandler(ABC):
 
-    def __init__(self):
-        self._data = None
-
     @abc.abstractmethod
-    def load_data(self, path):
-        pass
-
-    @abc.abstractmethod
-    def convert_into_dataframe(self):
+    def load_data(self, path: str) -> pl.DataFrame:
         pass
