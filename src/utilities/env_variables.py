735e2a7d087b83862ecfcee17eaabbf1f89267cb
Alberto Soutullo Rendo <alberto.soutullo639@gmail.com>
2022-11-27 16:42:16 +0100

    Refactored and removed unnecessary system variables

--- src/utilities/env_variables.py
+++ src/utilities/env_variables.py
@@ -1,10 +1,11 @@
-# Workaround while docker is not set up
 
-# binary_path = "wsl /mnt/d/Projects/status/Rust/consensus-research/target/release-opt/snow-family"
-binary_path = "snow-family"
-# configuration_settings = "configuration_settings.json"
-configuration_settings = "shared/configuration_settings.json"
-arguments_schema_path = "src/utilities/files/schemas/arguments_schema.json"
-configuration_schema_path = "src/utilities/files/schemas/snow_family_configuration_schema.json"
-shared_folder = "shared"
-input_settings = "shared/config_example.json"
+# Binary
+BINARY_PATH = "snow-family"
+CONFIGURATION_SETTINGS = "shared/configuration_settings.json"
+
+# Schemas
+ARGUMENTS_SCHEMA_PATH = "src/utilities/files/schemas/arguments_schema.json"
+CONFIGURATION_SCHEMA_PATH = "src/utilities/files/schemas/snow_family_configuration_schema.json"
+
+# Shared folder
+SHARED_FOLDER = "shared/"
