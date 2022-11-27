4018bf53ad48f6df462f5fad091e4e06c26098e2
Alberto Soutullo Rendo <alberto.soutullo639@gmail.com>
2022-11-27 16:44:05 +0100

    Simulation configuration is now parsed through command line

--- src/main.py
+++ src/main.py
@@ -4,13 +4,13 @@ import typer
 # Project Imports
 from src.simulation_runner.runner import run_simulation
 from src.utilities.files.json.simulation_config_parser import SimulationConfigParser
-from src.utilities.env_variables import input_settings
 
 
 def main(
         run_type: str = typer.Option(..., "--run", "-r"),
+        configuration_file: str = typer.Option(..., "--configuration-file", "-cf")
 ):
-    parser = SimulationConfigParser(input_settings)
+    parser = SimulationConfigParser(configuration_file)
     arguments_config, simulation_config, plotter_config = parser.read_content()
     # Calls depending on json config
     if run_type == "simulation":
